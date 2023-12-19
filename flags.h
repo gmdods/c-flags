9277e6c0c7c39fdc19dbe81cb3c3de175bb9af9a
gmd01bit <gmd01bit@gmail.com>
2023-12-19 14:00:05 +0200

    :sparkles: Numeric flags and safe dispatch

--- flags.h
+++ flags.h
@@ -10,6 +10,7 @@
 
 enum flag {
 	FLAG_BOOL,
+	FLAG_UINT,
 	FLAG_STRING,
 };
 
@@ -25,6 +26,7 @@ static inline void flags_default(const struct option table[static 1]) {
 	for (size_t t = 0; (entry = table[t]).variable != NULL; ++t) {
 		switch (entry.type) {
 		case FLAG_BOOL: *((bool *) entry.variable) = false; break;
+		case FLAG_UINT: *((unsigned *) entry.variable) = 0; break;
 		case FLAG_STRING:
 			*((const char **) entry.variable) = NULL;
 			break;
@@ -33,36 +35,43 @@ static inline void flags_default(const struct option table[static 1]) {
 	}
 }
 
-static inline size_t flag_assign(struct option entry, size_t argn,
-				 const char * argv[static argn]) {
+static inline short flag_assign(struct option entry, size_t argn,
+				const char * argv[static argn]) {
+
+	const char * endp = {0};
 	switch (entry.type) {
 	case FLAG_BOOL: *((bool *) entry.variable) = true; return 0;
+	case FLAG_UINT:
+		endp = argv[0] + strcspn(argv[0], "0123456789");
+		if (!*endp) return -1;
+		*((unsigned *) entry.variable) = atoi(endp);
+		return 0;
 	case FLAG_STRING:
-		if (0 == argn) return 0;
+		if (0 == argn) return -1;
 		*((const char **) entry.variable) = argv[1];
 		return 1;
-	default: return 0;
+	default: return -1;
 	}
 }
 
-static inline size_t flag_dispatch(const struct option table[static 1],
-				   size_t argn,
-				   const char * argv[static argn]) {
+static inline short flag_dispatch(const struct option table[static 1],
+				  size_t argn, const char * argv[static argn]) {
 	const char * arg = argv[0];
 	struct option entry = {0};
-	size_t delta = 0;
 	if (!strcmp(arg, "--")) {
 		for (size_t t = 0; (entry = table[t]).variable != NULL; ++t) {
 			if (strcmp(arg + 2, entry.long_name)) continue;
-			delta += flag_assign(entry, argn - delta, argv + delta);
+			short delta = flag_assign(entry, argn, argv);
+			if (delta) return delta;
 		}
 	} else if (arg[0] == '-') {
 		for (size_t t = 0; (entry = table[t]).variable != NULL; ++t) {
 			if (!strchr(arg + 1, entry.short_name)) continue;
-			delta += flag_assign(entry, argn - delta, argv + delta);
+			short delta = flag_assign(entry, argn, argv);
+			if (delta) return delta;
 		}
 	}
-	return delta;
+	return 0;
 }
 
 void flags(const struct option table[static 1], size_t argc,
@@ -70,8 +79,11 @@ void flags(const struct option table[static 1], size_t argc,
 	if (argc <= 1) return;
 
 	flags_default(table);
+
 	for (size_t i = 1; i != argc; ++i) {
-		i += flag_dispatch(table, argc - i, argv + i);
+		short delta = flag_dispatch(table, argc - i, argv + i);
+		if (delta < 0) return;
+		i += delta;
 	}
 }
 
--- test/unittest.c
+++ test/unittest.c
@@ -10,7 +10,9 @@
 #endif /* ifndef UNITTEST_MAIN */
 
 #define ARGC_ARGV(n, ...) \
-	n + 1, (const char * [n + 2]) { __FILE__, __VA_ARGS__, NULL }
+	n + 1, (const char * [n + 2]) { \
+		[0] = __FILE__, __VA_ARGS__, [n + 1] = NULL \
+	}
 
 unittest("boolean parsing") {
 	bool all = 0, verbose = 0, quiet = 0;
@@ -35,22 +37,26 @@ unittest("boolean parsing") {
 }
 
 unittest("string parsing") {
-	bool verbose = 0, quiet = 0;
+	bool verbose = 0;
 	char * string = NULL;
+	unsigned opt = 0;
 	const struct option table[] = {
 	    {'v', "verbose", FLAG_BOOL, &verbose},
-	    {'q', "quiet", FLAG_BOOL, &quiet},
 	    {'c', "compile", FLAG_STRING, &string},
+	    {'O', "opt", FLAG_UINT, &opt},
 	    {0},
 	};
 	static const char * compile = "print(1 + 2)";
 
 	flags(table, ARGC_ARGV(1, "-v"));
-	ensure(string == NULL), ensure(verbose), ensure(!quiet);
+	ensure(string == NULL), ensure(verbose), ensure(opt == 0);
 
 	flags(table, ARGC_ARGV(2, "-c", compile));
-	ensure(!strcmp(string, compile)), ensure(!verbose), ensure(!quiet);
+	ensure(!strcmp(string, compile)), ensure(!verbose), ensure(opt == 0);
 
 	flags(table, ARGC_ARGV(3, "-v", "-c", compile));
-	ensure(!strcmp(string, compile)), ensure(verbose), ensure(!quiet);
+	ensure(!strcmp(string, compile)), ensure(verbose), ensure(opt == 0);
+
+	flags(table, ARGC_ARGV(4, "-v", "-c", compile, "-O3"));
+	ensure(!strcmp(string, compile)), ensure(verbose), ensure(opt == 3);
 }
