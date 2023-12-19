e74976f373d5bf288bdc58661b0f8f07bb9ac731
gmd01bit <gmd01bit@gmail.com>
2023-12-19 13:44:52 +0200

    :art: Better test macros

--- test/unittest.c
+++ test/unittest.c
@@ -9,28 +9,30 @@
 
 #endif /* ifndef UNITTEST_MAIN */
 
+#define ARGC_ARGV(n, ...) \
+	n + 1, (const char * [n + 2]) { __FILE__, __VA_ARGS__, NULL }
+
 unittest("boolean parsing") {
 	bool all = 0, verbose = 0, quiet = 0;
 	const struct option table[] = {
 	    {'a', "all", FLAG_BOOL, &all},
 	    {'v', "verbose", FLAG_BOOL, &verbose},
-	    {'q', "quiet", FLAG_BOOL,&quiet},
+	    {'q', "quiet", FLAG_BOOL, &quiet},
 	    {0},
 	};
 
-	flags(table, 3, (const char * [4]){__FILE__, "-a", "-v", NULL});
-	ensure(all); ensure(verbose); ensure(!quiet);
+	flags(table, ARGC_ARGV(2, "-a", "-v"));
+	ensure(all), ensure(verbose), ensure(!quiet);
 
-	flags(table, 2, (const char * [3]){__FILE__, "-av", NULL});
-	ensure(all); ensure(verbose); ensure(!quiet);
+	flags(table, ARGC_ARGV(1, "-av"));
+	ensure(all), ensure(verbose), ensure(!quiet);
 
-	flags(table, 2, (const char * [3]){__FILE__, "-q", NULL});
-	ensure(!all); ensure(!verbose); ensure(quiet);
+	flags(table, ARGC_ARGV(1, "-q"));
+	ensure(!all), ensure(!verbose), ensure(quiet);
 
-	flags(table, 3, (const char * [4]){__FILE__, "-a", "--verbose", NULL});
-	ensure(all); ensure(verbose); ensure(!quiet);
+	flags(table, ARGC_ARGV(2, "-a", "--verbose"));
+	ensure(all), ensure(verbose), ensure(!quiet);
 }
-
 
 unittest("string parsing") {
 	bool verbose = 0, quiet = 0;
@@ -42,14 +44,13 @@ unittest("string parsing") {
 	    {0},
 	};
 	static const char * compile = "print(1 + 2)";
-	static const char * regex = "[0-9]+";
 
-	flags(table, 2, (const char * [3]){__FILE__, "-v", NULL});
-	ensure(string == NULL); ensure(verbose); ensure(!quiet);
+	flags(table, ARGC_ARGV(1, "-v"));
+	ensure(string == NULL), ensure(verbose), ensure(!quiet);
 
-	flags(table, 3, (const char * [4]){__FILE__, "-c", compile, NULL});
-	ensure(!strcmp(string, compile)); ensure(!verbose); ensure(!quiet);
+	flags(table, ARGC_ARGV(2, "-c", compile));
+	ensure(!strcmp(string, compile)), ensure(!verbose), ensure(!quiet);
 
-	flags(table, 4, (const char * [5]){__FILE__, "-v", "-c", compile, NULL});
-	ensure(!strcmp(string, compile)); ensure(verbose); ensure(!quiet);
+	flags(table, ARGC_ARGV(3, "-v", "-c", compile));
+	ensure(!strcmp(string, compile)), ensure(verbose), ensure(!quiet);
 }
